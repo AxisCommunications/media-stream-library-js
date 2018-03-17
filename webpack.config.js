c777b4b65c5467c7d1cf0f0cf0c623a100dcae3b
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-03-17 12:08:13 +0100

    add babelrc:false instead of file

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            babelrc: false
+          }
         }
       }
     ]
