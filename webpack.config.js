ebe33ca9560ee3447e8a9f6073db6d1154e83a7a
Oskar Damkjaer <OskarDamkjaer@users.noreply.github.com>
2018-03-17 15:10:08 +0100

    Prevent global .babelrc from overriding webpack (#12)
    * Prevent global .babelrc from overriding webpack
    
    If no .babelrc is present, webpack with look for a global .babelrc
    before using preset in its config. To avoid config duplication the
    option `babelrc: false` is added to the webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['env'],
+            babelrc: false
           }
         }
       }
