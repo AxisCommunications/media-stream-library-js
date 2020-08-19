d16fb67f9f80fdd655c3343c2608521139d0f246
Cole Willison <cwillison94@gmail.com>
2020-08-19 15:59:37 -0400

    Internet explorer (ES5 example)
    Added simple rtsp stream example that is compatible with Internet explorer (tested using Axis camera)

---
+++ examples/browser/camera/IE-ES5-RTSP-simple-stream/rtsp-player.js
@@ -0,0 +1,83 @@
+(function () {
+	var pipelines = mediaStreamLibrary.pipelines;
+
+	/*
+	This example works with an ES5 version of the media stream library. The purpose of this example is to demonstrate Internet explorer (IE) functionality.
+	(Unfortunately, some of us still have to 'support' it :( )
+	
+	*/
+
+	/*
+	var username = 'root';
+	var password = '<some_super_secret>';
+	*/
+
+	function authorize(host) {
+		var auth = new XMLHttpRequest();
+		// this will force a popup requesting a username and password
+		// to bypass the popup give the username and password directly in the request (uncomment)
+		auth.open("GET", 'http://' + host + '/axis-cgi/usergroup.cgi', false/*, username, password*/);
+		auth.send(null);
+	}
+
+	function play(host, encoding) {
+		const videoEl = document.querySelector('video');
+		const canvasEl = document.querySelector('canvas');
+		// Grab a reference to the video element
+		var Pipeline;
+		var mediaElement;
+		if (encoding === 'h264') {
+			Pipeline = pipelines.Html5VideoPipeline
+			mediaElement = videoEl
+			// hide the other output
+			videoEl.style.display = '';
+			canvasEl.style.display = 'none';
+		} else {
+			Pipeline = pipelines.Html5CanvasPipeline
+			mediaElement = canvasEl
+			// hide the other output
+			videoEl.style.display = 'none';
+			canvasEl.style.display = '';
+		}
+
+		var config = {};
+		config.ws = {
+			uri: 'ws://' + host + '/rtsp-over-websocket'
+		};
+		config.rtsp = {
+			uri: 'rtsp://' + host + '/axis-media/media.amp?videocodec=' + encoding
+		}
+		config.mediaElement = mediaElement;
+		// Setup a new pipeline
+		var pipeline = new Pipeline(config);
+
+		pipeline.ready.then(function () {
+			pipeline.rtsp.play();
+		});
+
+		return pipeline;
+	}
+
+	var pipeline;
+
+	document.addEventListener('DOMContentLoaded', function () {
+		init();
+	}, false);
+
+	function init() {
+		var playButton = document.querySelector('#play-button');
+		console.log(playButton);
+		playButton.addEventListener('click', function (e) {
+			var device = document.querySelector('#device');
+			var host = device.value || device.placeholder;
+			var encoding = document.querySelector('input[name=encoding]:checked').id;
+
+			console.log(host, encoding);
+			authorize(host);
+			console.log("done authorization");
+
+			pipeline = play(host, encoding);
+		});
+	}
+
+})();
