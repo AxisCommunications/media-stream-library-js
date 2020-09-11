9432d96da9dd83dc9bcc73e8ae4db08910622fed
Steven Vancoillie <steven.vancoillie@axis.com>
2020-09-11 16:42:00 +0200

    fix: improve release script
     - generate changelog after tag
     - apply patch if no version scheduled

--- overlay/sbin/release.sh
+++ overlay/sbin/release.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+# Generate new commit
+if [[ ! -d .yarn/version ]]; then
+  yarn version patch
+fi
+yarn version apply --all
 VERSION=$(jq -r .version package.json)
-bash sbin/changelog.sh > CHANGELOG.md && git add CHANGELOG.md
 git add -u
 git commit -m "v$VERSION"
+git tag "v$VERSION"
+
+# Update changelog (requires tag)
+bash sbin/changelog.sh > CHANGELOG.md && git add CHANGELOG.md
+git tag -d "v$VERSION"
+git commit --amend --no-edit
 git tag -a -m "v$VERSION" "v$VERSION"
