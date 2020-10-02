90f6ece28b58cb00bb9667302509213945b1153a
Steven Vancoillie <steven.vancoillie@axis.com>
2020-10-02 10:15:19 +0200

    fix: disable explicit module boundary types

--- player/lib/global.d.ts
+++ player/lib/global.d.ts
@@ -1,7 +1,6 @@
 import { ResizeObserver } from '@juggle/resize-observer'
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/interface-name-prefix
   interface Window {
     readonly ResizeObserver: typeof ResizeObserver
   }
