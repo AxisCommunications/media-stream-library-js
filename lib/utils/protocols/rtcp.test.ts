26b5c4942c2923fff21288e503cd4d6ee513d39e
Gustav Tiger <gustav.tiger@axis.com>
2021-11-03 11:47:54 +0100

    feat(rtp): RTCP parsing
    Extend media stream library with basic RTCP parsing. Supports all things
    from RTP: A Transport Protocol for Real-Time Applications
    (RFC 3550).
    
    Unknown package types will be parsed as generic RTCP messages which can
    be further parsed by the application if need.
    
    Includes a small test suite for RTCP message parsing.

--- lib/utils/protocols/rtcp.test.ts
+++ lib/utils/protocols/rtcp.test.ts
@@ -1,38 +1,222 @@
-import { rtcpBuffers } from './fixtures'
-import { count, packetType, padding, SR, version } from './rtcp'
+import {
+  rtcpAPPBuffers,
+  rtcpBYEBuffers,
+  rtcpRRBuffers,
+  rtcpSDESBuffers,
+  rtcpSRBuffers,
+} from './fixtures'
+import { parseRtcp } from './rtcp'
 
 describe('Rtcp parsing', () => {
-  for (const buffer of rtcpBuffers) {
-    it('is parsed correctly', () => {
-      expect(version(buffer)).toEqual(2)
-      expect(padding(buffer)).toEqual(false)
-      expect(count(buffer)).toEqual(0)
-      expect(packetType(buffer)).toEqual(SR.packetType)
-      expect(SR.syncSource(buffer)).toEqual(4197078097)
+  it('parsed SR correctly', () => {
+    expect(parseRtcp(rtcpSRBuffers[0])).toEqual({
+      version: 2,
+      padding: false,
+      count: 0,
+      packetType: 200,
+      length: 6,
+
+      syncSource: 4090175489,
+      ntpMost: 2209022881,
+      ntpLeast: 3942779706,
+      rtpTimestamp: 37920,
+      sendersPacketCount: 158,
+      sendersOctetCount: 39816,
+      reports: [],
     })
-  }
 
-  it('should expose correct ntpMost', () => {
-    expect(SR.ntpMost(rtcpBuffers[0])).toEqual(3668306118)
-    expect(SR.ntpMost(rtcpBuffers[1])).toEqual(3668306118)
-    expect(SR.ntpMost(rtcpBuffers[2])).toEqual(3668306131)
+    expect(parseRtcp(rtcpSRBuffers[1])).toEqual({
+      version: 2,
+      padding: false,
+      count: 3,
+      packetType: 200,
+      length: 24,
+
+      syncSource: 4090175489,
+      ntpMost: 2209022881,
+      ntpLeast: 3942779706,
+      rtpTimestamp: 37920,
+      sendersPacketCount: 158,
+      sendersOctetCount: 39816,
+      reports: [
+        {
+          syncSource: 1,
+          fractionLost: 4,
+          cumulativeNumberOfPacketsLost: 10,
+          extendedHighestSequenceNumberReceived: 232,
+          interarrivalJitter: 5,
+          lastSRTimestamp: 6,
+          delaySinceLastSR: 7,
+        },
+        {
+          syncSource: 2,
+          fractionLost: 4,
+          cumulativeNumberOfPacketsLost: 11,
+          extendedHighestSequenceNumberReceived: 233,
+          interarrivalJitter: 8,
+          lastSRTimestamp: 9,
+          delaySinceLastSR: 10,
+        },
+        {
+          syncSource: 3,
+          fractionLost: 4,
+          cumulativeNumberOfPacketsLost: 12,
+          extendedHighestSequenceNumberReceived: 234,
+          interarrivalJitter: 11,
+          lastSRTimestamp: 12,
+          delaySinceLastSR: 13,
+        },
+      ],
+    })
   })
 
-  it('should have the correct ntpLeast', () => {
-    expect(SR.ntpLeast(rtcpBuffers[0])).toEqual(445534137)
-    expect(SR.ntpLeast(rtcpBuffers[1])).toEqual(445534137)
-    expect(SR.ntpLeast(rtcpBuffers[2])).toEqual(1936420365)
+  it('parsed RR correctly', () => {
+    expect(parseRtcp(rtcpRRBuffers[0])).toEqual({
+      version: 2,
+      padding: false,
+      count: 0,
+      packetType: 201,
+      length: 1,
+
+      syncSource: 460716364,
+      reports: [],
+    })
+
+    expect(parseRtcp(rtcpRRBuffers[1])).toEqual({
+      version: 2,
+      padding: false,
+      count: 3,
+      packetType: 201,
+      length: 19,
+
+      syncSource: 460716364,
+      reports: [
+        {
+          syncSource: 1,
+          fractionLost: 4,
+          cumulativeNumberOfPacketsLost: 10,
+          extendedHighestSequenceNumberReceived: 232,
+          interarrivalJitter: 5,
+          lastSRTimestamp: 6,
+          delaySinceLastSR: 7,
+        },
+        {
+          syncSource: 2,
+          fractionLost: 4,
+          cumulativeNumberOfPacketsLost: 11,
+          extendedHighestSequenceNumberReceived: 233,
+          interarrivalJitter: 8,
+          lastSRTimestamp: 9,
+          delaySinceLastSR: 10,
+        },
+        {
+          syncSource: 3,
+          fractionLost: 4,
+          cumulativeNumberOfPacketsLost: 12,
+          extendedHighestSequenceNumberReceived: 234,
+          interarrivalJitter: 11,
+          lastSRTimestamp: 12,
+          delaySinceLastSR: 13,
+        },
+      ],
+    })
   })
 
-  it('should have the correct senders packet count', () => {
-    expect(SR.sendersPacketCount(rtcpBuffers[0])).toEqual(1734)
-    expect(SR.sendersPacketCount(rtcpBuffers[1])).toEqual(1734)
-    expect(SR.sendersPacketCount(rtcpBuffers[2])).toEqual(1840)
+  it('parsed SDES correctly', () => {
+    expect(parseRtcp(rtcpSDESBuffers[0])).toEqual({
+      version: 2,
+      padding: false,
+      count: 1,
+      packetType: 202,
+      length: 12,
+
+      syncSource: 3650993623,
+      sourceDescriptions: [
+        {
+          source: 3650993623,
+          items: [
+            [1, 'user2503145766@host-29205952'],
+            [6, 'GStreamer'],
+          ],
+        },
+      ],
+    })
+
+    expect(parseRtcp(rtcpSDESBuffers[1])).toEqual({
+      version: 2,
+      padding: false,
+      count: 2,
+      packetType: 202,
+      length: 12,
+
+      syncSource: 1,
+      sourceDescriptions: [
+        {
+          source: 1,
+          items: [
+            [1, 'CNAME1'],
+            [8, 'C1', 'V1'],
+          ],
+        },
+        {
+          source: 2,
+          items: [
+            [1, 'CNAME2'],
+            [8, 'C2', 'V2'],
+            [8, 'C3', 'V3'],
+          ],
+        },
+      ],
+    })
   })
 
-  it('should have the correct senders octet count', () => {
-    expect(SR.sendersOctetCount(rtcpBuffers[0])).toEqual(1278825)
-    expect(SR.sendersOctetCount(rtcpBuffers[1])).toEqual(1278825)
-    expect(SR.sendersOctetCount(rtcpBuffers[2])).toEqual(1357000)
+  it('parsed BYE correctly', () => {
+    expect(parseRtcp(rtcpBYEBuffers[0])).toEqual({
+      version: 2,
+      padding: false,
+      count: 1,
+      packetType: 203,
+      length: 1,
+
+      sources: [650497119],
+      reason: undefined,
+    })
+
+    expect(parseRtcp(rtcpBYEBuffers[1])).toEqual({
+      version: 2,
+      padding: false,
+      count: 0,
+      packetType: 203,
+      length: 0,
+
+      sources: [],
+      reason: undefined,
+    })
+
+    expect(parseRtcp(rtcpBYEBuffers[2])).toEqual({
+      version: 2,
+      padding: false,
+      count: 3,
+      packetType: 203,
+      length: 5,
+
+      sources: [1, 2, 3],
+      reason: 'Lost',
+    })
+  })
+
+  it('parsed APP correctly', () => {
+    expect(parseRtcp(rtcpAPPBuffers[0])).toEqual({
+      version: 2,
+      padding: false,
+      count: 5,
+      packetType: 204,
+      length: 4,
+
+      subtype: 5,
+      source: 42,
+      name: 'Life',
+      data: Buffer.from([0, 1, 2, 3, 42, 42, 42, 42]),
+    })
   })
 })
--- lib/utils/protocols/rtcp.ts
+++ lib/utils/protocols/rtcp.ts
@@ -1,3 +1,4 @@
+import { MessageType, RtcpMessage } from '../../components/message'
 import { POS } from '../bits'
 
 // Real Time Control Protocol (RTCP)
@@ -12,25 +13,61 @@ Common RTCP packed header:
 header |V=2|P|    RC   |   PT=SR=200   |             length            |
        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 */
-
-export const version = (buffer: Buffer) => {
-  return buffer[0] >>> 6
+export enum RTCPPacketType {
+  SR = 200,
+  RR = 201,
+  SDES = 202,
+  BYE = 203,
+  APP = 204,
 }
 
-export const padding = (buffer: Buffer) => {
-  return !!(buffer[0] & POS[2])
+export interface Rtcp {
+  readonly version: number
+  readonly padding: boolean
+  readonly count: number
+  readonly packetType: RTCPPacketType | number
+  readonly length: number
 }
 
-export const count = (buffer: Buffer) => {
-  return buffer[0] & 0x1f
+const parseBase = (buffer: Buffer): Rtcp => ({
+  version: buffer[0] >>> 6,
+  padding: !!(buffer[0] & POS[2]),
+  count: buffer[0] & 0x1f,
+  packetType: buffer.readUInt8(1),
+  length: buffer.readUInt16BE(2),
+})
+
+export const parseRtcp = (
+  buffer: Buffer,
+): Rtcp | RtcpSR | RtcpRR | RtcpSDES | RtcpBye | RtcpApp => {
+  const base = parseBase(buffer)
+
+  switch (base.packetType) {
+    case RTCPPacketType.SR:
+      return parseSR(buffer, base)
+    case RTCPPacketType.RR:
+      return parseRR(buffer, base)
+    case RTCPPacketType.SDES:
+      return parseSDES(buffer, base)
+    case RTCPPacketType.BYE:
+      return parseBYE(buffer, base)
+    case RTCPPacketType.APP:
+      return parseAPP(buffer, base)
+    default:
+      return base
+  }
 }
 
-export const packetType = (buffer: Buffer) => {
-  return buffer[1]
-}
-
-export const length = (buffer: Buffer) => {
-  return buffer.readUInt16BE(2)
+export const rtcpMessageFromBuffer = (
+  channel: number,
+  buffer: Buffer,
+): RtcpMessage => {
+  return {
+    type: MessageType.RTCP,
+    data: buffer,
+    channel,
+    rtcp: parseRtcp(buffer),
+  }
 }
 
 /*
@@ -75,31 +112,64 @@ block  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 export const SR = {
   packetType: 200,
-
-  syncSource: (buffer: Buffer) => {
-    return buffer.readUInt32BE(4)
-  },
-
-  ntpMost: (buffer: Buffer) => {
-    return buffer.readUInt32BE(8)
-  },
-
-  ntpLeast: (buffer: Buffer) => {
-    return buffer.readUInt32BE(12)
-  },
-
-  rtpTimestamp: (buffer: Buffer) => {
-    return buffer.readUInt32BE(16)
-  },
-
-  sendersPacketCount: (buffer: Buffer) => {
-    return buffer.readUInt32BE(20)
-  },
-
-  sendersOctetCount: (buffer: Buffer) => {
-    return buffer.readUInt32BE(24)
-  },
 }
+
+export interface RtcpReportBlock {
+  readonly syncSource: number
+  readonly fractionLost: number
+  readonly cumulativeNumberOfPacketsLost: number
+  readonly extendedHighestSequenceNumberReceived: number
+  readonly interarrivalJitter: number
+  readonly lastSRTimestamp: number
+  readonly delaySinceLastSR: number
+}
+
+const parseReportBlocks = (
+  count: number,
+  buffer: Buffer,
+  offset: number,
+): RtcpReportBlock[] => {
+  const reports: RtcpReportBlock[] = []
+  for (let reportNumber = 0; reportNumber < count; reportNumber++) {
+    const o = offset + reportNumber * 24
+    reports.push({
+      syncSource: buffer.readUInt32BE(o + 0),
+      fractionLost: buffer.readUInt8(o + 4),
+      cumulativeNumberOfPacketsLost: buffer.readUIntBE(o + 5, 3),
+      extendedHighestSequenceNumberReceived: buffer.readUInt32BE(o + 8),
+      interarrivalJitter: buffer.readUInt32BE(o + 12),
+      lastSRTimestamp: buffer.readUInt32BE(o + 16),
+      delaySinceLastSR: buffer.readUInt32BE(o + 20),
+    })
+  }
+  return reports
+}
+
+export interface RtcpSR extends Rtcp {
+  readonly version: RTCPPacketType.SR
+
+  readonly syncSource: number
+  readonly ntpMost: number
+  readonly ntpLeast: number
+  readonly rtpTimestamp: number
+  readonly sendersPacketCount: number
+  readonly sendersOctetCount: number
+  readonly reports: readonly RtcpReportBlock[]
+}
+
+const parseSR = (buffer: Buffer, base: Rtcp): RtcpSR => ({
+  ...base,
+  syncSource: buffer.readUInt32BE(4),
+  ntpMost: buffer.readUInt32BE(8),
+  ntpLeast: buffer.readUInt32BE(12),
+  rtpTimestamp: buffer.readUInt32BE(16),
+  sendersPacketCount: buffer.readUInt32BE(20),
+  sendersOctetCount: buffer.readUInt32BE(24),
+  reports: parseReportBlocks(base.count, buffer, 28),
+})
+
+export const isRtcpSR = (rtcp: Rtcp): rtcp is RtcpSR =>
+  rtcp.packetType === RTCPPacketType.SR
 
 /*
 RR: Receiver Report RTCP Packet
@@ -131,9 +201,21 @@ block  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 */
 
-export const RR = {
-  packetType: 201,
+export interface RtcpRR extends Rtcp {
+  readonly version: RTCPPacketType.RR
+
+  readonly syncSource: number
+  readonly reports: readonly RtcpReportBlock[]
 }
+
+const parseRR = (buffer: Buffer, base: Rtcp): RtcpRR => ({
+  ...base,
+  syncSource: buffer.readUInt32BE(4),
+  reports: parseReportBlocks(base.count, buffer, 8),
+})
+
+export const isRtcpRR = (rtcp: Rtcp): rtcp is RtcpRR =>
+  rtcp.packetType === RTCPPacketType.RR
 
 /*
 SDES: Source Description RTCP Packet
@@ -155,9 +237,84 @@ chunk  |                          SSRC/CSRC_2                          |
        +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
 */
 
-export const SDES = {
-  packetType: 202,
+export enum SDESItem {
+  CNAME = 1,
+  NAME = 2,
+  EMAIL = 3,
+  PHONE = 4,
+  LOC = 5,
+  TOOL = 6,
+  NOTE = 7,
+  PRIV = 8,
 }
+
+export interface RtcpSDESBlock {
+  readonly source: number
+  readonly items: Array<[number, string] | [SDESItem.PRIV, string, string]>
+}
+
+export interface RtcpSDES extends Rtcp {
+  readonly version: RTCPPacketType.SDES
+
+  readonly syncSource: number
+  readonly sourceDescriptions: readonly RtcpSDESBlock[]
+}
+
+const parseSDES = (buffer: Buffer, base: Rtcp): RtcpSDES => {
+  const sourceDescriptions: RtcpSDESBlock[] = []
+  let offset = 4
+  for (let block = 0; block < base.count; block++) {
+    const chunk: RtcpSDESBlock = {
+      source: buffer.readUInt32BE(offset),
+      items: [],
+    }
+    offset += 4
+
+    while (true) {
+      const itemType = buffer.readUInt8(offset++)
+
+      if (itemType === 0) {
+        // start next block at word boundary
+        if (offset % 4 !== 0) {
+          offset += 4 - (offset % 4)
+        }
+        break
+      }
+
+      const length = buffer.readUInt8(offset++)
+
+      if (itemType === SDESItem.PRIV) {
+        const prefixLength = buffer.readUInt8(offset)
+        const prefix = buffer.toString(
+          'utf8',
+          offset + 1,
+          offset + 1 + prefixLength,
+        )
+        const value = buffer.toString(
+          'utf8',
+          offset + 1 + prefixLength,
+          offset + length,
+        )
+        chunk.items.push([SDESItem.PRIV, prefix, value])
+      } else {
+        const value = buffer.toString('utf8', offset, offset + length)
+        chunk.items.push([itemType, value])
+      }
+
+      offset += length
+    }
+    sourceDescriptions.push(chunk)
+  }
+
+  return {
+    ...base,
+    syncSource: buffer.readUInt32BE(4),
+    sourceDescriptions,
+  }
+}
+
+export const isRtcpSDES = (rtcp: Rtcp): rtcp is RtcpSDES =>
+  rtcp.packetType === RTCPPacketType.SDES
 
 /*
 BYE: Goodbye RTCP Packet
@@ -175,9 +332,35 @@ BYE: Goodbye RTCP Packet
       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 */
 
-export const BYE = {
-  packetType: 203,
+export interface RtcpBye extends Rtcp {
+  readonly version: RTCPPacketType.BYE
+
+  readonly sources: number[]
+  readonly reason?: string
 }
+
+const parseBYE = (buffer: Buffer, base: Rtcp): RtcpBye => {
+  const sources: number[] = []
+  for (let block = 0; block < base.count; block++) {
+    sources.push(buffer.readUInt32BE(4 + 4 * block))
+  }
+
+  let reason
+  if (base.length > base.count) {
+    const start = 4 + 4 * base.count
+    const length = buffer.readUInt8(start)
+    reason = buffer.toString('utf-8', start + 1, start + 1 + length)
+  }
+
+  return {
+    ...base,
+    sources,
+    reason,
+  }
+}
+
+export const isRtcpBye = (rtcp: Rtcp): rtcp is RtcpBye =>
+  rtcp.packetType === RTCPPacketType.BYE
 
 /*
 APP: Application-Defined RTCP Packet
@@ -196,6 +379,24 @@ APP: Application-Defined RTCP Packet
 
 */
 
-export const APP = {
-  packetType: 204,
+export interface RtcpApp extends Rtcp {
+  readonly version: RTCPPacketType.APP
+
+  readonly subtype: number
+  readonly source: number
+  readonly name: string
+  readonly data: Buffer
 }
+
+const parseAPP = (buffer: Buffer, base: Rtcp): RtcpApp => {
+  return {
+    ...base,
+    subtype: base.count,
+    source: buffer.readUInt32BE(4),
+    name: buffer.toString('ascii', 8, 12),
+    data: buffer.slice(12),
+  }
+}
+
+export const isRtcpApp = (rtcp: Rtcp): rtcp is RtcpApp =>
+  rtcp.packetType === RTCPPacketType.APP
