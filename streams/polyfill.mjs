12cf74ac068a3a8848764e312317d9a9ddd501a0
Rikard Tegnander <rikard.tegnander@gmail.com>
2024-12-17 07:35:51 +0100

    Adds polyfill for UInt8Array and remove buffer

--- streams/polyfill.mjs
+++ streams/polyfill.mjs
@@ -1,4 +1,5 @@
-import { Buffer } from 'buffer'
-window.Buffer = Buffer
 import * as process_browser from 'process/browser'
 window.process_browser = process_browser
+
+import uint from 'core-js-pure/actual/typed-array/uint8-array.js'
+Uint8Array = uint
